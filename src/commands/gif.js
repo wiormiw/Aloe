1be77e6d314fffe5b06aea902c7396907921e57f
wiormiw <wwijoyo2@gmail.com>
2021-06-29 22:15:08 +0700

    optimize gif-feature

--- src/commands/gif.js
+++ src/commands/gif.js
@@ -4,15 +4,19 @@ const { default: fetch } = require('node-fetch');
 const random = require('../utils/randomFunction');
 
 module.exports = async function (message, args) {
-    let keywords
+    let keywords, index;
     if (args.length > 0) {
         keywords = args.join(" ");
     }
     let url = `https://g.tenor.com/v1/search?q=${keywords}&key=${process.env.GIF_TOKEN}`;
     let response = await fetch(url);
     let json = await response.json();
-
-    // didieu rek random wae?
-    const index = random(json.results);
+    let words = keywords.split(" ");
+    if (words[words.length-1] == "random") {
+        index = random(json.results);
+    } else {
+        let option = isNaN(parseInt(words[words.length-1])) ? 0 : parseInt(words[words.length-1])
+        index = option;
+    }
     message.channel.send(json.results[index].url);
 }
